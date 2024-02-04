98002955c0af69af4ad13b47566a861927e2103a
Atharva <atharva4b@gmail.coim>
2024-02-04 14:48:44 +0530

    Atharva Linking

--- app/src/main/java/com/example/onlinemeetingapp/HomePage.java
+++ app/src/main/java/com/example/onlinemeetingapp/HomePage.java
@@ -1,14 +1,27 @@
 package com.example.onlinemeetingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomePage extends AppCompatActivity {
 
+    CardView profile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+    profile=findViewById(R.id.profilebtn);
+
+    profile.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startActivity(new Intent(HomePage.this, Profile.class));
+        }
+    });
     }
 }
--- app/src/main/java/com/example/onlinemeetingapp/Login.java
+++ app/src/main/java/com/example/onlinemeetingapp/Login.java
@@ -2,13 +2,43 @@ package com.example.onlinemeetingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class Login extends AppCompatActivity {
 
+    TextInputEditText mail,pass;
+    TextView gotoregister;
+    RelativeLayout login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+    gotoregister=findViewById(R.id.gotoRegister);
+    login=findViewById(R.id.btnLogin);
+    mail=findViewById(R.id.mail);
+    pass=findViewById(R.id.pass);
+
+    String email=mail.getText().toString().trim();
+    String password=pass.getText().toString().trim();
+
+    gotoregister.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startActivity(new Intent(Login.this,Register.class));
+        }
+    });
+    login.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    });
     }
 }
