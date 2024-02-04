56c13c14eb55307785102859ac77230f2cfa4212
Atharva <atharva4b@gmail.coim>
2024-02-04 15:02:48 +0530

    Atharva Linking (Register)

--- app/src/main/java/com/example/onlinemeetingapp/Register.java
+++ app/src/main/java/com/example/onlinemeetingapp/Register.java
@@ -1,14 +1,27 @@
 package com.example.onlinemeetingapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
 public class Register extends Activity {
+
+    TextView gotologin;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+    gotologin=findViewById(R.id.lnkLogin);
+
+    gotologin.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startActivity(new Intent(Register.this,Login.class));
+        }
+    });
     }
 }
