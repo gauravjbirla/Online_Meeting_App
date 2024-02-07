9f9290d726374c9892251320a6e11f0895d40889
Atharva <atharva4b@gmail.coim>
2024-02-07 15:52:24 +0530

    Meetingsnave

--- app/src/main/java/com/example/onlinemeetingapp/Meetings.java
+++ app/src/main/java/com/example/onlinemeetingapp/Meetings.java
@@ -2,13 +2,41 @@ package com.example.onlinemeetingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class Meetings extends AppCompatActivity {
 
+
+    private LinearLayout home, settings, contacts;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meetings);
+
+        home=findViewById(R.id.nevhome);
+        contacts=findViewById(R.id.contacts);
+        settings=findViewById(R.id.settings);
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Meetings.this, HomePage.class));
+            }
+        });
+        contacts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Meetings.this, Contacts.class));
+            }
+        });
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Meetings.this, Settings.class));
+            }
+        });
     }
 }
