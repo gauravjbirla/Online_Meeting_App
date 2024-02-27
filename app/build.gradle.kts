ae10b9afe995c2b794d39dd69ecb6198dbad65a0
Atharv <atharvkale22@gmail.com>
2024-02-27 19:52:47 +0530

    done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,5 +41,4 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     //jitsi
-    implementation("org.jitsi.react:jitsi-meet-sdk:8.2.2") { isTransitive=true }
-}
+    implementation("org.jitsi.react:jitsi-meet-sdk:2.9.0") { var transitive = true }}
